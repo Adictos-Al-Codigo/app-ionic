2cb54362f21266be56b4ba4a74a25852ed022110
Celorio2001 <jahir.celorio2001@gmail.com>
2023-07-27 17:13:30 -0500

    Añadiendo el modal a la vista buscar

--- src/app/pages/buscar/buscar.page.ts
+++ src/app/pages/buscar/buscar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 import { TheMoviesDBService } from 'src/app/services/the-movies-db.service';
 
 
@@ -9,7 +10,7 @@ import { TheMoviesDBService } from 'src/app/services/the-movies-db.service';
 })
 export class BuscarPage implements OnInit {
 
-  constructor(private theMoviesDBService:TheMoviesDBService) { }
+  constructor(private theMoviesDBService:TheMoviesDBService, private animationCtrl:AnimationController) { }
 
 
 
@@ -37,5 +38,34 @@ export class BuscarPage implements OnInit {
       }
     })
   }
+
+  
+  enterAnimation = (baseEl: HTMLElement) => {
+    const root = baseEl.shadowRoot;
+
+    const backdropAnimation = this.animationCtrl
+      .create()
+      .addElement(root!.querySelector('ion-backdrop')!)
+      .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
+
+    const wrapperAnimation = this.animationCtrl
+      .create()
+      .addElement(root!.querySelector('.modal-wrapper')!)
+      .keyframes([
+        { offset: 0, opacity: '0', transform: 'scale(0)' },
+        { offset: 1, opacity: '0.99', transform: 'scale(1)' },
+      ]);
+
+    return this.animationCtrl
+      .create()
+      .addElement(baseEl)
+      .easing('ease-out')
+      .duration(500)
+      .addAnimation([backdropAnimation, wrapperAnimation]);
+  };
+
+  leaveAnimation = (baseEl: HTMLElement) => {
+    return this.enterAnimation(baseEl).direction('reverse');
+  };
 
 }
